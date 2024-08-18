10c49c8ac295535ac5634769d757bc740adc5066
Felipe Lima <flimas1985@gmail.com>
2024-08-18 16:20:17 -0300

    First Commit

---
+++ src/task/task.controller.ts
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { TaskDto } from './task.dto';
+import { TaskService } from './task.service';
+
+@Controller('task')
+export class TaskController {
+
+    constructor(private readonly taskService: TaskService) { }
+
+    @Post()
+    create(@Body() task: TaskDto) {
+        this.taskService.create(task);
+    }
+
+    @Get('/:id')
+    findById(@Param('id') id: string) {
+        return this.taskService.findById(id);
+    }
+
+    @Get()
+    findAll() {
+        
+    }
+    
+    @Put()
+    update(@Body() task: TaskDto) {
+        this.taskService.update(task);
+    }
+
+    @Delete('/:id')
+    remove(@Param('id') id: string) {
+        return this.taskService.remove(id);
+    }
+}
